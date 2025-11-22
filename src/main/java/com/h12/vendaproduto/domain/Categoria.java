f2eb0b563347c142cf19512ed9b832c131587a95
hysleiman12 <hysleiman12>
2025-11-22 19:23:45 -0300

    Banco de dados H2 finalizado e criação da base de dados

--- src/main/java/com/h12/vendaproduto/domain/Categoria.java
+++ src/main/java/com/h12/vendaproduto/domain/Categoria.java
@@ -4,6 +4,13 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Categoria implements Serializable {
 
     /**
@@ -13,7 +20,8 @@ public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 	
 	
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
